93de2bc99bf341e7ace32e3a3fc41233852e880e
changchingchi <chi.changching@gmail.com>
2015-03-09 15:50:46 -0700

    add new model for JSON fetch

--- App.js
+++ App.js
@@ -15,4 +15,10 @@ angular.module('nameApp',[])
 	$scope.removeName = function(name){
 		$scope.namearray.splice($scope.namearray.indexOf(name),1);
 	}
+
+})
+
+.controller('CountryCtrl', function($scope, $http){
+		$http.get('http://www.w3schools.com/website/Customers_JSON.php')
+			.success(function(response){$scope.names = response;})
 })
